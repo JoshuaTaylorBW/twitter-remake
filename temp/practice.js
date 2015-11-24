2d1d692dcc2bbe2e876377c1eb66540b86b3906f
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2015-11-24 12:42:31 -0700

    Finished Triangle Excercise

---
+++ temp/practice.js
@@ -0,0 +1,8 @@
+var myArray = ["Yoda", "Vader", "Skywalker", "Leia"];
+
+
+for (var i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
+
+myArray.splice(1,1);
---
+++ tmep/junk.js
@@ -0,0 +1,7 @@
+var a  = [1,2,3,4];
+
+console.log(a);
+
+a[2] = 7;
+
+console.log(a);
